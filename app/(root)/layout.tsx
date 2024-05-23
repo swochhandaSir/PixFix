a7821eefb796ec572e9ceeed0f28e40226fbe25c
Swochhanda Jangam <swochhandajangam@gmail.com>
2024-05-23 11:30:14 +0545

    Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../globals.css";
-import Sidebar from '@/components/shared/sidebar'
+import Sidebar from '@/components/shared/Sidebar';
 import MobileNav from '@/components/shared/MobileNav';
 
 const Layout = ({ children }: {children: React.ReactNode }) => {
